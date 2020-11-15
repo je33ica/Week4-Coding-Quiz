cba37ae6b706c26cae498c2ee504bc738cb0bb84
je33ica <je331687@gmail.com>
2020-11-15 21:08:06 +0000

    questions array added

---
+++ script.js
@@ -0,0 +1,30 @@
+//questions in an array with answer matching
+let questions = [
+    {
+      question: "A loop that never ends is referred to as a(n)_________.",
+      choices: ["While loop", "Infinte loop", "Recursive loop", "For loop"],
+      answer: "Infinte loop",
+    },
+    {
+      question: "What is hoisting in Javascript?",
+      choices: [
+        "Best practice for accessing variables before they are declared ",
+        "The default behavior of moving all declarations to the top of the current scope",
+        "Changing how the code looks using indentation",
+        "Minimising code into a smaller size file ",
+      ],
+      answer:
+        "The default behavior of moving all declarations to the top of the current scope",
+    },
+    {
+      question: "When was javascript first created?",
+      choices: ["2001", "1990", "1988", "1995"],
+      answer: "1995",
+    },
+  
+    {
+      question: " Where does a javascript tag go in a html file go?",
+      choices: ["<div>", "<script>", "<container>", "<head>"],
+      answer: "<script>",
+    },
+  ];
